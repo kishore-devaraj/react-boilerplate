8687ac8690823b8f41d7a2a0c5fb656c72950529
kishore-devaraj <kishoregrylls@gmail.com>
2018-06-26 11:56:10 +0530

    Required npm libraries installed for Non-CRA to work

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.css';
+
+const App = () => {
+    return (
+        <div className="app-container">
+            <h1>Hello React</h1>
+        </div>
+    )
+}
+
+export default App;
