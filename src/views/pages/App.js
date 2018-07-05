097ac82b5ef355640beb3c72c661b96abcafd490
kishore-devaraj <kishoregrylls@gmail.com>
2018-07-05 13:02:31 +0530

    Added instructions in the landing page

--- src/views/pages/App.js
+++ src/views/pages/App.js
@@ -4,7 +4,15 @@ import './App.css';
 const App = () => {
     return (
         <div className="app-container">
-            <h1>Hello React</h1>
+            <h1>React says Hello!</h1>
+            <div className="instructions-container">
+                <ul>
+                    <li>Open the page App.js and starting editing.</li>
+                    <li>Keep your components, assets in view folder.</li>
+                    <li>Code your application logic state in redux folder.</li>
+                    <li>Env variables and api endpoints should be placed in config file.</li>
+                </ul>
+            </div>            
         </div>
     )
 }
